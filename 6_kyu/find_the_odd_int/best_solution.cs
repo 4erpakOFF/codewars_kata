d53402bf9f5436b170dba9bb3ebbc6512525ec55
4erpakOFF <pro198ha@mail.ru>
2019-08-25 00:33:54 +0300

    find_the_odd added

---
+++ 6_kyu/find_the_odd_int/best_solution.cs
@@ -0,0 +1,12 @@
+﻿namespace Solution
+{
+    class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            int result = 0;
+            foreach (var num in seq) result ^= num;
+            return result;
+        }
+    }
+}
---
+++ 6_kyu/find_the_odd_int/first_solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace Solution
+{
+    class Kata
+    {
+        public static int find_it(int[] seq)
+        {
+            var dict = new Dictionary<int, int>();
+            foreach (var s in seq)
+            {
+                if (dict.ContainsKey(s))
+                    dict[s]++;
+                else
+                    dict.Add(s, 1);
+            }
+
+            foreach (var pair in dict)
+                if (pair.Value % 2 == 1)
+                    return pair.Key;
+
+            return -1;
+        }
+
+    }
+}
