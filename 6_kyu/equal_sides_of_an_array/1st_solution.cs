c972ed978b985d4f31855d9e20f41bc38cf26ea4
4erpakOFF <pro198ha@mail.ru>
2019-08-25 00:41:58 +0300

    Equal Sides Of An Array added

---
+++ 6_kyu/equal_sides_of_an_array/1st_solution.cs
@@ -0,0 +1,20 @@
+﻿public class Kata
+{
+    public static int FindEvenIndex(int[] arr)
+    {
+        if (arr.Length == 0) return -1;
+        var left = new long[arr.Length];
+        var right = new long[arr.Length];
+        left[0] = arr[0];
+        right[arr.Length - 1] = arr[arr.Length - 1];
+
+        for (int i = 1; i < arr.Length; i++) left[i] = left[i - 1] + arr[i];
+        for (int j = arr.Length - 2; j >= 0; j--) right[j] = right[j + 1] + arr[j];
+
+        for (int i = 0; i < arr.Length; i++)
+            if (left[i] == right[i])
+                return i;
+
+        return -1;
+    }
+}
---
+++ 6_kyu/equal_sides_of_an_array/2nd_solution.cs
@@ -0,0 +1,18 @@
+﻿public class Kata
+{
+    public static int FindEvenIndex(int[] arr)
+    {
+        if (arr.Length == 0) return -1;
+        var rSum = 0;
+        var lSum = 0;
+        foreach (var a in arr) rSum += a;
+        for (var i = 0; i < arr.Length; i++)
+        {
+            lSum += arr[i];
+            if (lSum == rSum) return i;
+            rSum -= arr[i];
+        }
+
+        return -1;
+    }
+}
